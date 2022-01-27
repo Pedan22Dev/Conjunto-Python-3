5d8655cc11f4fe7cdaf26b8bb06c9d50459d405f
Henrique Rocha <hrtecprog@gmail.com>
2022-01-27 16:31:34 -0300

    Backup

---
+++ Cap4_ep2/Dicionario/dicionario.py
@@ -0,0 +1,15 @@
+usuarios = {}
+print(usuarios)
+
+usuarios = {
+    "chaves" : ["Chaves do 8", "24/12/2017", "recep 01"],
+    "quico" : ["Quico Das Flores", "20/12/2017", "raio_x 03"]
+    }
+
+print(usuarios)
+
+usuarios["florinda"] = ["Dona Florinda", "24/12/2017", "Raiox_01"]
+print(usuarios)
+
+print('######----######')
+print(usuarios.get("quico"))
---
+++ Cap4_ep3/Dicionario/funcoes.py
@@ -0,0 +1,14 @@
+def perguntar():
+    return   input("O que deseja realizar?\n" +
+            "<I> - Para Inserir um usuário\n" +
+            "<P> - Para Pesquisar um usuário\n" +
+            "<E> - Para Excluir um usuário\n" +
+            "<L> - Para Listar um usuário\n"
+).upper()
+
+def inserir(dicionario):
+            chave =  input("Digite o Login: ").upper()
+            nome =  input("Digite o Nome: ").upper()
+            data =  input("Digite o última data de acesso: ").upper()
+            estacao =  input("Última estação acessada: ").upper()
+            dicionario[chave] = [nome, data, estacao] 
